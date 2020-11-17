4f6336c3f08722c934b1d7225cd45ef92d63f9ae
Purvi jha <51432532+Purvicapternal@users.noreply.github.com>
2020-11-17 01:46:11 -0800

    Update todo.js

--- src/component/todo.js
+++ src/component/todo.js
@@ -2,8 +2,12 @@ import React from 'react'
 
 export default function Todo() {
     return (
-        <div>
-            hiiiiii from Todo
+        <div >
+             <hr />
+            <div style={{color:"#eeeeee",fontSize:"2.5em",margin:"auto",marginTop:"100px"}}>
+            <h1>Coming Soon</h1>
+
+            </div>
         </div>
     )
 }
