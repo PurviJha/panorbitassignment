cf13d5cbd3e713c84ec5d79c11525006dc23ea61
Purvi jha <51432532+Purvicapternal@users.noreply.github.com>
2020-11-17 01:43:49 -0800

    Update navbar.js

--- src/component/navbar.js
+++ src/component/navbar.js
@@ -1,7 +1,10 @@
-import React from 'react'
+import React,{useState} from 'react'
 import Avatar from '@material-ui/core/Avatar';
-
+import {useLocation} from 'react-router-dom'
 export default function Navbar(props) {
+    const location=useLocation()
+    console.log("locattion",location.pathname.split("/").pop())
+    
     return (
         <div>
             <div style={{
@@ -10,16 +13,14 @@ export default function Navbar(props) {
                 marginTop: "45px",
                 //minHeight: "10%",  
             }}>
-                 <h3>{ props.brand}
+                 <h3>{ location.pathname.split("/").pop().toUpperCase()}
         </h3>
         <div style={{ display: 'flex',
                 flexDirection: "row",marginLeft:'75%'}}>
-        <Avatar alt={props.data.name} src={props.data.profilepicture } style={{height:"30px",width:"30px"}}  />
+                    <Avatar  alt={props.data.name} src={props.data.profilepicture } style={{height:"30px",width:"30px"}}  />
             <p>{props.data.name}</p>
         </div>
-            
             </div>
-            <hr />
         </div>
     )
 }
