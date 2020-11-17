458a3fab766f4265481238c15ed440180f17c9fd
Purvi jha <51432532+Purvicapternal@users.noreply.github.com>
2020-11-17 01:44:57 -0800

    Update profile.js

--- src/component/profile.js
+++ src/component/profile.js
@@ -1,10 +1,9 @@
-import React,{useEffect,useState} from 'react'
+import React from 'react'
 import GoogleMapReact from 'google-map-react'
-
 export default function Profile(props) {
-    const data=props.location.state.data 
-    console.log("props",props) 
-        const defaultProps = {
+    const data = window.userInfo
+    console.log("data1234567890", data)
+    const defaultProps = {
         center: {
             lat: -37.3159,
             lng: 81.1496
@@ -12,46 +11,61 @@ export default function Profile(props) {
         zoom: 11
     }
     return (
-        <div style={{ display: "flex" }}>
-            <div style={{ width: " 40%", float: "left", textAlign: "center" }}>
-                {console.log("data",data)}
-                <img src={data.profilepicture} alt={data.id}
-                    style={{ width: "150px", height: "150px", borderRadius: "50%" }} />
-                <h3>{data.name}</h3>
-                <p style={{ Color: "#A0A0A0" }}>Username : {data.name}</p>
-                <p style={{ Color: "#A0A0A0" }}>e-mail : {data.email}</p>
-                <p style={{ Color: "#A0A0A0" }}>phone : {data.phone}</p>
-                <p style={{ Color: "#A0A0A0" }}>website : {data.website}</p>
-                <hr style={{ width: "70%" }} />
-                <p style={{ Color: "#A0A0A0" }}>Company</p>
-                <p style={{ Color: "#A0A0A0" }}>Name : {data.company.name}</p>
-                <p style={{ Color: "#A0A0A0" }}>CatchPhrase : {data.company.catchPhrase}</p>
-                <p style={{ Color: "#A0A0A0" }}>bs : {data.company.bs}</p>
-                <br /><br />
-            </div>
-            <div style={{
-                borderLeft: " 1px solid",
-                width: "1px",
-                height: "550px",
-                top: "12px"
-            }}></div>
-            <div style={{ width: " 40%", float: "right", marginLeft: "20px" }}>
-                <p style={{ Color: "#A0A0A0" }}>Address :</p>
-                <p style={{ Color: "#A0A0A0" }}>Street   : {data.address.street}</p>
-                <p style={{ Color: "#A0A0A0" }}>Suite   : {data.address.suite}</p>
-                <p style={{ Color: "#A0A0A0" }}>City   : {data.address.city}</p>
-                <p style={{ Color: "#A0A0A0" }}>Zipcode   : {data.address.zipcode}</p>
-                <div style={{ height: '300px', width: '500px', borderRadius: "20px" }}>
-                    <GoogleMapReact
-                        //bootstrapURLKeys={{ key: /* YOUR KEY HERE */ }}
-                        defaultCenter={defaultProps.center}
-                        defaultZoom={defaultProps.zoom}
-                    >
-                    </GoogleMapReact>
-                    <p>lat:{data.address.geo.lat} lng:{data.address.geo.lng} </p>
-                </div>
+        <div>
+            <hr />
+            <div style={{ display: "flex" }}>
 
+                <div style={{ width: " 40%", float: "left", }}>
+                    <div style={{ textAlign: "center" }}>
+                        <img src={data.profilepicture} alt={data.id}
+                            style={{ width: "150px", height: "150px", borderRadius: "50%" }} />
+                        <h3>{data.name}</h3>
+                    </div>
+                    <div style={{ marginLeft: "70px" }}>
+                        <p ><span style={{ color: "#A0A0A0" }}>Username :</span>{data.username}</p>
+                        <p ><span style={{ color: "#A0A0A0" }}>e-mail :</span>{data.email}</p>
+                        <p ><span style={{ color: "#A0A0A0" }}>Phone :</span>{data.phone}</p>
+                        <p ><span style={{ color: "#A0A0A0" }}>Website :</span>{data.website}</p>
+                        <hr style={{ width: "70%", marginLeft: "-2px" }} />
+                        <p style={{ marginLeft: "70px", color: "#A0A0A0" }}>Company</p>
+                        <p ><span style={{ color: "#A0A0A0" }}>Name :</span>{data.company.name}</p>
+                        <p ><span style={{ color: "#A0A0A0" }}>CatchPhrase :</span>{data.company.catchPhrase}</p>
+                        <p ><span style={{ color: "#A0A0A0" }}>bs :</span>{data.company.bs}</p>
+
+                    </div>
+                </div>
+                <div style={{
+                    borderLeft: " 1px solid",
+                    width: "1px",
+                    height: "550px",
+                    top: "12px"
+                }}></div>
+                <div style={{ width: " 40%", float: "right", marginLeft: "20px" }}>
+                    <p style={{ color: "#A0A0A0" }}>Address:</p>
+                    <div style={{ marginLeft: "70px" }}>
+                        <p ><span style={{ color: "#A0A0A0" }}>Street :</span>{data.address.street}</p>
+                        <p ><span style={{ color: "#A0A0A0" }}>Suite :</span>{data.address.suite}</p>
+                        <p ><span style={{ color: "#A0A0A0" }}>City :</span>{data.address.city}</p>
+                        <p ><span style={{ color: "#A0A0A0" }}>Zipcode :</span>{data.address.zipcode}</p>
+                    </div>
+                    <div style={{ height: '300px', width: '500px', borderRadius: "20px" }}>
+                        <GoogleMapReact
+                            //bootstrapURLKeys={{ key: /* YOUR KEY HERE */ }}
+                            defaultCenter={defaultProps.center}
+                            defaultZoom={defaultProps.zoom}
+                        >
+                        </GoogleMapReact>
+                        <div style={{ marginLeft: "63%" }}>
+                            <p ><span style={{ color: "#A0A0A0" }}>lat :</span>{data.address.geo.lat}{"  "}
+                                <span style={{ color: "#A0A0A0" }}>lng:</span>{data.address.geo.lng} </p>
+
+                        </div>
+                    </div>
+
+                </div>
             </div>
         </div>
+
     )
 }
+
